994973bab51144c74c61ac3c82407cbf3f1d44d4
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-06 14:50:27 +0100

    Declared Comparable conformance.

---
+++ Integers/Integer/Integer+Comparable.swift
@@ -0,0 +1,23 @@
+
+extension Integer: Comparable {
+    
+    @inlinable
+    public static func < (lhs: Integer, rhs: Integer) -> Bool {
+        fatalError()
+    }
+    
+    @inlinable
+    public static func <= (lhs: Integer, rhs: Integer) -> Bool {
+        fatalError()
+    }
+    
+    @inlinable
+    public static func >= (lhs: Integer, rhs: Integer) -> Bool {
+        fatalError()
+    }
+    
+    @inlinable
+    public static func > (lhs: Integer, rhs: Integer) -> Bool {
+        fatalError()
+    }
+}
