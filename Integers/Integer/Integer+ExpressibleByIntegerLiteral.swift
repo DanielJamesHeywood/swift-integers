1243ace8c59c684bcdca8e9a934d3f3add757aa9
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-06 14:12:46 +0100

    Declared conformance to ExpressibleByIntegerLiteral.

---
+++ Integers/Integer/Integer+ExpressibleByIntegerLiteral.swift
@@ -0,0 +1,27 @@
+
+extension Integer: ExpressibleByIntegerLiteral {
+    
+    public typealias IntegerLiteralType = StaticBigInt
+    
+    @inlinable
+    public init(integerLiteral value: StaticBigInt) {
+        _words = Array(
+            unsafeUninitializedCapacity: value.bitWidth._dividedRoundingUp(by: UInt.bitWidth),
+            initializingWith: { buffer, initializedCount in
+                for index in buffer.indices {
+                    buffer.initializeElement(at: index, to: value[index])
+                }
+                initializedCount = buffer.count
+            }
+        )
+    }
+}
+
+extension BinaryInteger {
+    
+    @inlinable
+    internal func _dividedRoundingUp(by other: Self) -> Self {
+        let (quotient, remainder) = quotientAndRemainder(dividingBy: other)
+        return remainder == 0 ? quotient : quotient + 1
+    }
+}
