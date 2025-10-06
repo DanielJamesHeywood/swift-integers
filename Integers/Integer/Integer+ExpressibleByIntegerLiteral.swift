cbb4a5d95c0412765725b5a9b8282b8f44e6f684
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-06 14:32:31 +0100

    Updated _dividedRoundingUp(by:) to prevent division by 0 and to correctly round quotients.

--- Integers/Integer/Integer+ExpressibleByIntegerLiteral.swift
+++ Integers/Integer/Integer+ExpressibleByIntegerLiteral.swift
@@ -21,7 +21,8 @@ extension BinaryInteger {
     
     @inlinable
     internal func _dividedRoundingUp(by other: Self) -> Self {
+        precondition(other != 0)
         let (quotient, remainder) = quotientAndRemainder(dividingBy: other)
-        return remainder == 0 ? quotient : quotient + 1
+        return remainder != 0 && signum() == other.signum() ? quotient + 1 : quotient
     }
 }
