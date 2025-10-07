ed8d10dd6533d2b55ebdeae6c4fb5083f8a85bf2
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-07 16:45:21 +0100

    Declared conformance to Codable.

---
+++ Integers/Integer/Integer+Decodable.swift
@@ -0,0 +1,8 @@
+
+extension Integer: Decodable {
+    
+    @inlinable
+    public init(from decoder: any Decoder) throws {
+        fatalError()
+    }
+}
---
+++ Integers/Integer/Integer+Encodable.swift
@@ -0,0 +1,8 @@
+
+extension Integer: Encodable {
+    
+    @inlinable
+    public func encode(to encoder: any Encoder) throws {
+        fatalError()
+    }
+}
