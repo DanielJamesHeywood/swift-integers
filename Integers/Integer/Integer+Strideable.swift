8a8e5fdc5c846722ce2fb5047fbfb1fcfdd88894
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-07 13:06:16 +0100

    Expanded Strideable declaration to include underscored methods.

--- Integers/Integer/Integer+Strideable.swift
+++ Integers/Integer/Integer+Strideable.swift
@@ -12,4 +12,12 @@ extension Integer: Strideable {
     public func advanced(by n: Integer) -> Integer {
         fatalError()
     }
+    
+    @inlinable
+    public static func _step(
+      after current: (index: Int?, value: Integer),
+      from start: Integer, by distance: Integer
+    ) -> (index: Int?, value: Integer) {
+        fatalError()
+    }
 }
