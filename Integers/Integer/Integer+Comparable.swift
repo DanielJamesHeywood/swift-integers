c3ceac540bfafdaa3f1747a28f8339834db0131a
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-07 13:56:09 +0100

    Continued implementing Comparable.

--- Integers/Integer/Integer+Comparable.swift
+++ Integers/Integer/Integer+Comparable.swift
@@ -3,21 +3,45 @@ extension Integer: Comparable {
     
     @inlinable
     public static func < (lhs: Integer, rhs: Integer) -> Bool {
-        fatalError()
+        return lhs._compare(to: rhs) == .lessThan
     }
     
     @inlinable
     public static func <= (lhs: Integer, rhs: Integer) -> Bool {
-        fatalError()
+        return lhs._compare(to: rhs) != .greaterThan
     }
     
     @inlinable
     public static func >= (lhs: Integer, rhs: Integer) -> Bool {
-        fatalError()
+        return lhs._compare(to: rhs) != .lessThan
     }
     
     @inlinable
     public static func > (lhs: Integer, rhs: Integer) -> Bool {
+        return lhs._compare(to: rhs) == .greaterThan
+    }
+}
+
+extension Integer {
+    
+    @frozen
+    @usableFromInline
+    internal enum _ComparisonResult {
+        case lessThan
+        case greaterThan
+        case equalTo
+    }
+    
+    @inlinable
+    internal func _compare(to other: Integer) -> _ComparisonResult {
         fatalError()
+    }
+}
+
+extension Integer {
+    
+    @inlinable
+    internal var _isNegative: Bool {
+        return _words.last.unsafelyUnwrapped.leadingZeroBitCount == 0
     }
 }
--- Integers/Integer/Integer.swift
+++ Integers/Integer/Integer.swift
@@ -5,11 +5,3 @@ public struct Integer {
     @usableFromInline
     internal var _words: [UInt]
 }
-
-extension Integer {
-    
-    @inlinable
-    internal var _isNegative: Bool {
-        return _words.last.unsafelyUnwrapped.leadingZeroBitCount == 0
-    }
-}
