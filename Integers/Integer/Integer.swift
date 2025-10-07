a969ddac8c4bfde0a486e6b0ecc317bb44737e2a
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-07 13:43:04 +0100

    Implemented utility function to determine if an integer is negative.

--- Integers/Integer/Integer.swift
+++ Integers/Integer/Integer.swift
@@ -5,3 +5,11 @@ public struct Integer {
     @usableFromInline
     internal var _words: [UInt]
 }
+
+extension Integer {
+    
+    @inlinable
+    internal var _isNegative: Bool {
+        return _words.last.unsafelyUnwrapped.leadingZeroBitCount == 0
+    }
+}
