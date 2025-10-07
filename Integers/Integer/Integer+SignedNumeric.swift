5c0cdb9bbfd70d970d187b6229d67758d6069e73
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-07 23:18:16 +0100

    Deleted - implementation for later implementation.

--- Integers/Integer/Integer+SignedNumeric.swift
+++ Integers/Integer/Integer+SignedNumeric.swift
@@ -3,35 +3,7 @@ extension Integer: SignedNumeric {
     
     @inlinable
     public prefix static func - (operand: Integer) -> Integer {
-        let words = operand._words
-        return Integer(
-            _words: Array(
-                unsafeUninitializedCapacity: words.count + 1,
-                initializingWith: { buffer, initializedCount in
-                    var partialValue: UInt = 0
-                    var overflow = false
-                    for index in words.indices {
-                        if overflow {
-                            (partialValue, overflow) = (0 as UInt).subtractingReportingOverflow(words[index])
-                            if overflow {
-                                partialValue &-= 1
-                            } else {
-                                (partialValue, overflow) = partialValue.subtractingReportingOverflow(1)
-                            }
-                        } else {
-                            (partialValue, overflow) = (0 as UInt).subtractingReportingOverflow(words[index])
-                        }
-                        buffer.initializeElement(at: index, to: partialValue)
-                    }
-                    if overflow {
-                        buffer.initializeElement(at: words.count, to: 0)
-                        initializedCount = words.count + 1
-                    } else {
-                        initializedCount = words.count
-                    }
-                }
-            )
-        )
+        fatalError()
     }
     
     @inlinable
