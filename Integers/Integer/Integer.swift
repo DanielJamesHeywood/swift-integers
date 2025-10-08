670ba76de25cd1a6a901fe76a8e804491039896a
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-09 00:14:56 +0100

    Declared utility function for standardization.

--- Integers/Integer/Integer.swift
+++ Integers/Integer/Integer.swift
@@ -4,6 +4,26 @@ public struct Integer {
     
     @usableFromInline
     internal var _words: [UInt]
+    
+    @inlinable
+    internal mutating func _standardize() {
+        if _words.last == .min {
+            repeat {
+                _words.removeLast()
+            } while _words.last == .min
+            if _words.last?.leadingZeroBitCount == 0 {
+                _words.append(.min)
+            }
+        }
+        if _words.last == .max {
+            repeat {
+                _words.removeLast()
+            } while _words.last == .max
+            if _words.last?.leadingZeroBitCount != 0 {
+                _words.append(.max)
+            }
+        }
+    }
 }
 
 extension Integer {
