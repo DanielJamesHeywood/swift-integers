29224bef0b0899609827fe0b597f4adbc05eba67
Daniel James Heywood <DanielJamesHeywood@icloud.com>
2025-10-09 16:15:33 +0100

    Implemented internal initializer.

--- Integers/Integer/Integer.swift
+++ Integers/Integer/Integer.swift
@@ -6,6 +6,13 @@ public struct Integer {
     internal var _words: [UInt]
     
     @inlinable
+    internal init(_words: [UInt]) {
+        precondition(!_words.isEmpty)
+        self._words = _words
+        _standardize()
+    }
+    
+    @inlinable
     internal mutating func _standardize() {
         if _words.last == .min {
             repeat {
