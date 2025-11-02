a16751b6f86e003992b728d5813f87035cfbee54
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-11-02 02:01:55 +0000

    Implemented description.

--- Integers/Integer/Integer+CustomStringConvertible.swift
+++ Integers/Integer/Integer+CustomStringConvertible.swift
@@ -3,6 +3,22 @@ extension Integer: CustomStringConvertible {
     
     @inlinable
     public var description: String {
-        fatalError()
+        var value = magnitude
+        var result = [] as [UInt8]
+        repeat {
+            let (quotient, remainder) = value.quotientAndRemainder(dividingBy: 10)
+            result.append(UInt8(("0" as Unicode.Scalar).value) &+ UInt8(remainder))
+            value = quotient
+        } while value != 0
+        if _isNegative {
+            result.append(UInt8(("-" as Unicode.Scalar).value))
+        }
+        result.reverse()
+        return String(
+            unsafeUninitializedCapacity: result.count,
+            initializingUTF8With: { buffer in
+                return buffer.initialize(fromContentsOf: result)
+            }
+        )
     }
 }
