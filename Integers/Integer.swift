63ebe9119ec2766247bb6a020ea8f1d46cfa00d4
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-11-24 00:46:31 +0000

    Implemented reserveCapacity optimizations &= and |=.

--- Integers/Integer.swift
+++ Integers/Integer.swift
@@ -286,6 +286,9 @@ extension Integer: BinaryInteger {
     
     @inlinable
     public static func &= (lhs: inout Integer, rhs: Integer) {
+        if lhs._words.count < rhs._words.count && !lhs._isNegative {
+            lhs._words.reserveCapacity(rhs._words.count)
+        }
         let lhsIsNegative = lhs._isNegative, rhsIsNegative = rhs._isNegative
         for (index, rhWord) in rhs._words.prefix(lhs._words.count)._enumeratedWithIndices() {
             lhs._words[index] &= rhWord
@@ -334,6 +337,9 @@ extension Integer: BinaryInteger {
     
     @inlinable
     public static func |= (lhs: inout Integer, rhs: Integer) {
+        if lhs._words.count < rhs._words.count && !lhs._isNegative {
+            lhs._words.reserveCapacity(rhs._words.count)
+        }
         let lhsIsNegative = lhs._isNegative, rhsIsNegative = rhs._isNegative
         for (index, rhWord) in rhs._words.prefix(lhs._words.count)._enumeratedWithIndices() {
             lhs._words[index] |= rhWord
