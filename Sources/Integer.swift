80e32a25a5769b1f8c92613bdc0ea30428d0c70e
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-02-04 10:20:40 +0000

    Improved bit width implementation.

--- Sources/Integer.swift
+++ Sources/Integer.swift
@@ -173,7 +173,7 @@ extension Integer: BinaryInteger {
     
     @inlinable
     public var bitWidth: Int {
-        _words.count * UInt.bitWidth
+        _isNegative ? _words.count * UInt.bitWidth - (~_words.last.unsafelyUnwrapped).leadingZeroBitCount : _words.count * UInt.bitWidth - _words.last.unsafelyUnwrapped.leadingZeroBitCount
     }
     
     @inlinable
